83143690476b95d5034afd323f051ce75fd3e9b6
Kyle Peatt <kpeatt@gmail.com>
2014-09-08 19:00:12 -0700

    Touch up Schemas

--- dist/components/select/select--inner-select.schema.js
+++ dist/components/select/select--inner-select.schema.js
@@ -1,6 +1,7 @@
 define([], function () {
 
     return {
+        'label': "Label",
         'select': $('select').append('<option>Value</option><option>Value</option>');
     };
 });
--- dist/components/select/select.schema.js
+++ dist/components/select/select.schema.js
@@ -1,9 +1,6 @@
 define([], function () {
 
     return {
-        'currentPage': '1',
-        'totalPage': '307',
-        'prevDisabled': true,
-        'nextDisabled': false
+        'select': $('select').append('<option>Value</option><option>Value</option>');
     };
 });
