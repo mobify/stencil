30437d2f76a1cb4ccb98a37278593029e7365d95
Aveline Thelen <aveline@mobify.com>
2014-10-23 14:38:22 -0700

    Create Breadcrumb schema

---
+++ dist/components/breadcrumb/breadcrumb.js
@@ -0,0 +1,11 @@
+define(['$'], function ($) {
+
+    return {
+        'breadcrumb': [
+            {'label': 'Home', 'url': '#'},
+            {'label': 'Homeware', 'url': '#'},
+            {'label': 'Kitchen', 'url': '#'},
+            {'label': 'Cooking & Baking', 'url': '#'}
+        ]
+    };
+});
