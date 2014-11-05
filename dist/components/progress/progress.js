7b78bdb03aee2e08f759d283a721fca3c0e53b62
Aveline Thelen <aveline@mobify.com>
2014-11-05 10:26:02 -0800

    Progress Component - Schema

---
+++ dist/components/progress/progress.js
@@ -0,0 +1,38 @@
+define([],function() {
+
+    return {
+        "maxStep": 0
+        "currentStep": 0,
+
+        "items": [
+            {
+                "index": "1",
+                "label": "",
+                "stepClass": "",
+                "isCurrentStep": false,
+                "isComplete": false
+            },
+            {
+                "index": "2",
+                "label": "",
+                "stepClass": "",
+                "isCurrentStep": false,
+                "isComplete": false
+            },
+            {
+                "index": "3",
+                "label": "",
+                "stepClass": "",
+                "isCurrentStep": false,
+                "isComplete": false
+            },
+            {
+                "index": "4",
+                "label": "",
+                "stepClass": "",
+                "isCurrentStep": false,
+                "isComplete": false
+            },
+        ]
+      }
+});
