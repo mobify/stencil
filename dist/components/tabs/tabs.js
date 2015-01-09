1c2974a81692e9b18eab68a897066a774c3aa3c2
Cole Sanderson <cole@mobify.me>
2015-01-09 10:59:31 -0800

    added aria-selected, aria-hidden and tabindex

--- dist/components/tabs/tabs.js
+++ dist/components/tabs/tabs.js
@@ -6,28 +6,36 @@ define(['$'], function ($) {
                 'id': 'home',
                 'title': 'Home',
                 'content': '<p>Content Here</p>',
-                'class': ''
+                'class': '',
+                'hidden': 'true',
+                'tabindex': '-1'
             },
 
             {
                 'id': 'homeware',
                 'title': 'Homeware',
                 'content': '<p>Content Here</p>',
-                'class': 'c--current'
+                'class': 'c--current',
+                'hidden': 'false',
+                'tabindex': '0'
             },
 
             {
                 'id': 'kitchen',
                 'title': 'Kitchen',
                 'content': '<p>Content Here</p>',
-                'class': ''
+                'class': '',
+                'hidden': 'true',
+                'tabindex': '-1'
             },
 
             {
                 'id': 'cooking-baking',
                 'title': 'Cooking & Baking',
                 'content': '<p>Content Here</p>',
-                'class': ''
+                'class': '',
+                'hidden': 'true',
+                'tabindex': '-1'
             }
         ]
     };
