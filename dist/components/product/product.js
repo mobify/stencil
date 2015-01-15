3a269fc94279ee5ff4b944a29f9423285f5baa4f
Cole Sanderson <cole@mobify.me>
2015-01-15 13:40:46 -0800

    Product component

---
+++ dist/components/product/product.js
@@ -0,0 +1,22 @@
+define(['$'], function ($) {
+
+    return {
+        'product': [
+            {
+                'isHighlight': true,
+                'title': 'Product Title',
+                'url': '#',
+                'image': {
+                    'url': '#'
+                    'path': '#',
+                    'title': 'Image Title'
+                },
+                'price': '$29.99',
+                'isDiscount': {
+                    'oldPrice': '$39.99',
+                    'discountPrice': '$29.99'
+                }
+            }
+        ]
+    };
+});
